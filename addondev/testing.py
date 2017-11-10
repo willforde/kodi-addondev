ce9be012d714a23fac899a7053a00eebdbbf6fb1
William Forde <willforde@gmail.com>
2017-11-10 21:08:07 +0000

    mock select dialog

--- addondev/testing.py
+++ addondev/testing.py
@@ -1,6 +1,10 @@
+import xbmcgui
 import xbmc
 
 
 def mock_keyboard(data):
     xbmc.Keyboard.mock_data.append(data)
 
+
+def mock_select_dialog(data):
+    xbmcgui.Dialog.mock_data["select"].append(data)
